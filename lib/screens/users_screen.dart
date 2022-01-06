11d1aee301e63c4e9297c448fbca3ffc21422291
Блажески Кирил <kiril.blazheski@students.finki.ukim.mk>
2022-01-06 16:03:28 +0100

    added location service

--- lib/screens/users_screen.dart
+++ lib/screens/users_screen.dart
@@ -2,9 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:jabber_app/service/firebase_service.dart';
 import 'package:jabber_app/widgets/user/user_card.dart';
 import 'package:jabber_app/widgets/user/users.dart';
+import 'package:location/location.dart';
 
 class UsersScreen extends StatelessWidget {
   const UsersScreen({Key? key}) : super(key: key);
+
+  void enableLocation() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+
+    print(_locationData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,10 +60,26 @@ class UsersScreen extends StatelessWidget {
                   ),
                   value: 'logout',
                 ),
+                DropdownMenuItem(
+                  child: Container(
+                    child: Row(children: [
+                      Icon(
+                        Icons.location_pin,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width: 8),
+                      Text('Enable location'),
+                    ]),
+                  ),
+                  value: 'location',
+                ),
               ],
               onChanged: (itemIdentifier) {
                 if (itemIdentifier == 'logout') {
                   FirebaseService.signOutUser();
+                }
+                if (itemIdentifier == 'location') {
+                  enableLocation();
                 }
               })
         ],
